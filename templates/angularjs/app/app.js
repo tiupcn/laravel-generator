8e3108b8a3fd603b484298aafcb388c13c6c99b7
胡鹏 <hup@tiup.us>
2017-06-15 18:03:20 +0800

    模板添加&修改&删除

--- templates/angularjs/app/app.js
+++ templates/angularjs/app/app.js
@@ -1,14 +1,17 @@
 var app = angular.module('app',[
-		require('angular-ui-router'),
-		require('oclazyload'),
-		require('restangular')
-	]);
+    require('angular-ui-router'),
+    require('oclazyload'),
+    require('restangular'),
+    require('angularjs-toaster'),
+    require('angular-ui-bootstrap')
+  ]);
 require('./config.lazyload.js');
 require('./config.route.js');
 require('./services/service.js');
+import 'angularjs-toaster/toaster.min.css';
 
 app.controller('AppCtrl',['$scope','$ocLazyLoad', function($scope, $ocLazyLoad){
-	$scope.app = {
+  $scope.app = {
     name: 'TiUP',
     version: '1.0.0',
     settings: {
--- templates/angularjs/app/services/service.js
+++ templates/angularjs/app/services/service.js
@@ -1,8 +1,17 @@
-//service.js
-
 angular.module('app')
-    .factory('R', function(Restangular) {
-      return Restangular.withConfig(function(RestangularConfigurer) {
-        RestangularConfigurer.setBaseUrl('/api/');
-      });
+  .factory('R', function(Restangular) {
+    return Restangular.withConfig(function(RestangularConfigurer) {
+      RestangularConfigurer.setBaseUrl('/api/');
+    }).addResponseInterceptor(function(data, operation, what, url, response, deferred){
+    	var extractedData;
+    		// .. to look for getList operations
+    	if(operation === "getList") {
+    		// .. and handle the data and meta data
+      	extractedData = data.data;
+      	extractedData.total = data.total;
+    	}else {
+        extractedData = data;
+      }
+      return extractedData;
     });
+  });
