e6b2764122c1768c800410f74602a6429d2da661
lizw <lizw@tiup.us>
2017-07-20 11:47:29 +0800

    theme

---
+++ config/theme.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+    "themeID" => 1,
+    "navbarHeaderColor" => "bg-white-only",
+    "navbarCollapseColor" => "bg-white-only",
+    "asideColor" => "bg-black",
+    "headerFixed" => true,
+    "asideFixed" => true,
+    "asideFolded" => false,
+    "asideDock" => false,
+    "container" => false
+);
